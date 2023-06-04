720c09e2a4fe8c2ce464f05631fcd10f60de169b
Antony TR <antonyetr@outlook.com>
2023-06-04 17:42:32 -0600

    agregue la funcion draw, que hace que los pixeles dentro del lienzo cambien de color, usando eventlistener con 'mouseover' en la funcion makeGrid

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const board = document.getElementById("board");
 const play_btn = document.getElementById("play_button");
-play_btn.addEventListener("click", () => makeGrid(25));
+play_btn.addEventListener("click", () => makeGrid(30));
 
 function makeGrid(pixels) {
     board.style.gridTemplateColumns = `repeat(${pixels}, 1fr)`
     board.style.gridTemplateRows = `repeat (${pixels}, 1fr)`
 
-    let totalDivs = pixels * pixels;
-
+    let totalDivs = (pixels * pixels);
     for (let i = 0; i < totalDivs; i++) {
         let div = document.createElement("div");
+        div.addEventListener("mouseover", () => draw(div, "red"))
         board.appendChild(div);
-        div.classList.add("grid")
+        div.classList.add("pixel")
     }
+}
+
+function draw(elem, color){
+    elem.style.backgroundColor = color
 }
