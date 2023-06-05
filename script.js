068296c1f8b9bf8e54b7348a69965f607caeddac
Antony TR <antonyetr@outlook.com>
2023-06-04 22:24:56 -0600

    Ya funcionan los botones clear y erase, agregue el boton rainbow para tener colores al azar

--- script.js
+++ script.js
@@ -7,8 +7,15 @@ const btn8 = document.getElementById("button_8");
 const btn16 = document.getElementById("button_16");
 const btn32 = document.getElementById("button_32");
 const btn64 = document.getElementById("button_64");
-const customSize = document.getElementById("input_size")
+const customSize = document.getElementById("input_size");
 let size = ""
+const eraserBtn = document.getElementById("eraser_button");
+const clearBtn = document.getElementById("clear_button");
+const rainbowBtn = document.getElementById("rainbow_button")
+
+rainbowBtn.addEventListener("click", () => {color = "rainbow"});
+clearBtn.addEventListener("click", () => {resetBoard()});
+eraserBtn.addEventListener("click", () => {setColor("white")})
 customSize.addEventListener("input", () => {
     setCustomGrid(customSize.value)
 })
@@ -45,11 +52,16 @@ function makeGrid(pixels) {
         let div = document.createElement("div");
         div.addEventListener("mouseover", () => draw(div))
         board.appendChild(div);
+        div.classList.add("grid")
     }
 }
 
 function draw(div){
+    if (color == "rainbow") {
+        div.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
     div.style.backgroundColor = color
+    div.style.border = "none"
 }
 
 function setColor(colorSelection){
