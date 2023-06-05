afb3d649e8613099d8ff45a684f58e576836551c
Antony TR <antonyetr@outlook.com>
2023-06-04 19:12:31 -0600

    ahora se puede elegir el color con un selector, el color se puede cambiar sin tener que recargar la pagina

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-const board = document.getElementById("board");
-const play_btn = document.getElementById("play_button");
-play_btn.addEventListener("click", () => makeGrid(30));
+const playBtn = document.getElementById("play_button");
+const colorInput = document.getElementById("color_picker")
+let color = "black";
+
+playBtn.addEventListener("click", () => makeGrid(30)); //TODO: que el usuario ingrese la cantidad de columnas y filas, para pasarlas a la funcion makeGrid
+colorInput.addEventListener("input", () => {setColor(colorInput.value)})
 
 function makeGrid(pixels) {
+    const board = document.getElementById("board");
     board.style.gridTemplateColumns = `repeat(${pixels}, 1fr)`
     board.style.gridTemplateRows = `repeat (${pixels}, 1fr)`
 
     let totalDivs = (pixels * pixels);
+
     for (let i = 0; i < totalDivs; i++) {
         let div = document.createElement("div");
-        div.addEventListener("mouseover", () => draw(div, "red"))
+        div.addEventListener("mouseover", () => draw(div))
         board.appendChild(div);
-        div.classList.add("pixel")
     }
 }
 
-function draw(elem, color){
-    elem.style.backgroundColor = color
+function draw(div){
+    div.style.backgroundColor = color
+}
+
+function setColor(colorSelection){
+    color = colorSelection;
 }
