b3f8b091a77d2dc6683c02fca691971fa614159d
Antony TR <antonyetr@outlook.com>
2023-06-04 22:44:36 -0600

    Refactorice el codigo, uso un foreach para agregar los eventlisteners a los botones, en lugar de hacerlo de uno en uno

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 const board = document.getElementById("board");
-const playBtn = document.getElementById("play_button");
 const colorInput = document.getElementById("color_picker")
 let color = "black";
-const btn4 = document.getElementById("button_4");
-const btn8 = document.getElementById("button_8");
-const btn16 = document.getElementById("button_16");
-const btn32 = document.getElementById("button_32");
-const btn64 = document.getElementById("button_64");
 const customSize = document.getElementById("input_size");
 let size = ""
 const eraserBtn = document.getElementById("eraser_button");
@@ -19,6 +13,13 @@ eraserBtn.addEventListener("click", () => {setColor("white")})
 customSize.addEventListener("input", () => {
     setCustomGrid(customSize.value)
 })
+colorInput.addEventListener("input", () => {setColor(colorInput.value)})
+
+const gridSizes = [4, 8, 16, 32, 64]
+gridSizes.forEach(size => {
+    const button = document.getElementById(`button_${size}`);
+    button.addEventListener("click", () => makeGrid(size));
+});
 
 function setCustomGrid(input){
     const warning = document.getElementById("warning_message")
@@ -31,15 +32,6 @@ function setCustomGrid(input){
     warning.classList.add("hidden")
     makeGrid(input)
 }
-
-btn4.addEventListener("click", () => makeGrid(4));
-btn8.addEventListener("click", () => makeGrid(8));
-btn16.addEventListener("click", () => makeGrid(16));
-btn32.addEventListener("click", () => makeGrid(32));
-btn64.addEventListener("click", () => makeGrid(64));
-
-
-colorInput.addEventListener("input", () => {setColor(colorInput.value)})
 
 function makeGrid(pixels) {
     resetBoard();
