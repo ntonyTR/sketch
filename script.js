8aeb2331fed7154089ab19d569022ccd11005800
Antony TR <antonyetr@outlook.com>
2023-06-04 20:11:32 -0600

    agregue botones para personalizar la cuadricula, hay un cuadro de texto para ingresar un valor especifico entre 2 y 100, si se ingresa un valor invalido, se resalta el mensaje para igresar un valor valido

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 const playBtn = document.getElementById("play_button");
 const colorInput = document.getElementById("color_picker")
 let color = "black";
+const btn4 = document.getElementById("button_4");
+const btn8 = document.getElementById("button_8");
+const btn16 = document.getElementById("button_16");
+const btn32 = document.getElementById("button_32");
+const btn64 = document.getElementById("button_64");
+const customSize = document.getElementById("custom_size")
+let size = ""
+customSize.addEventListener("input", () => {
+    setCustomGrid(customSize.value)
+})
 
-playBtn.addEventListener("click", () => makeGrid(30)); //TODO: que el usuario ingrese la cantidad de columnas y filas, para pasarlas a la funcion makeGrid
+function setCustomGrid(input){
+    const warning = document.getElementById("warning_message")
+    size = input;
+    if (isNaN(size)) {
+        warning.style.color = "red"
+        return
+    }
+    warning.style.color = "black"
+    makeGrid(input)
+}
+
+btn4.addEventListener("click", () => makeGrid(4));
+btn8.addEventListener("click", () => makeGrid(8));
+btn16.addEventListener("click", () => makeGrid(16));
+btn32.addEventListener("click", () => makeGrid(32));
+btn64.addEventListener("click", () => makeGrid(64));
+
+
 colorInput.addEventListener("input", () => {setColor(colorInput.value)})
 
 function makeGrid(pixels) {
